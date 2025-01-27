94bfa03c95c9063368b915cbb99b1412c8d62450
Alex Lanção <93793324+lancao2@users.noreply.github.com>
2025-01-27 16:57:29 -0300

    finalização aula 4

--- uc1/aula_4.js
+++ uc1/aula_4.js
@@ -155,10 +155,90 @@ switch (codigoDoProduto) {
 // Crie um programa que leia um número inteiro e verifique se ele está dentro do intervalo de 10 a 20 (inclusive). 
 // Se o número estiver dentro do intervalo, imprima "Número dentro do intervalo", caso contrário, imprima "Número fora do intervalo".
 
-let numero = 15;
+let numero = 9;
 
 if (numero >= 10 && numero <= 20) {
     console.log("Número dentro do intervalo");
 } else {
     console.log("Número fora do intervalo");
 }
+
+// QUESTÃO 7
+// Verificar se um número está dentro de dois intervalos
+// Crie um programa que leia um número inteiro e verifique se ele está dentro de dois intervalos:
+
+//     Entre 1 e 10 (inclusive)
+//     Entre 50 e 100 (inclusive).
+
+// Imprima "Número válido" se o número estiver em qualquer um desses intervalos, caso contrário, imprima "Número inválido".
+
+if (numero >= 1 && numero <= 10 || numero >= 50 && numero <= 100) {
+    console.log("Número válido");
+} else {
+    console.log("Número inválido");
+}
+
+// QUESTÃO 8
+//  Verificar se uma pessoa pode comprar bebidas alcoólicas
+// Crie um programa que leia a idade de uma pessoa e verifique se ela pode comprar bebidas alcoólicas. As regras são:
+
+// A pessoa pode comprar se tiver 18 anos ou mais
+// Se ela tiver entre 16 e 17 anos e estiver acompanhada de um responsável legal.
+
+// Imprima "Pode comprar bebidas" ou "Não pode comprar bebidas", dependendo do caso.
+
+let idadeComprador = 19
+let compradorAcompanhado = true
+
+if (idadeComprador >= 18 || (idadeComprador >= 16 && idadeComprador <= 17 && compradorAcompanhado)) {
+    console.log("Pode comprar bebidas");
+} else {
+    console.log("Não pode comprar bebidas");
+}
+
+// QUESTÃO 9
+// Verificar se uma pessoa pode tirar férias
+// Crie um programa que leia o tempo de serviço (em meses) de um funcionário e verifique se ele pode tirar férias. As condições para tirar férias são:
+
+//     Ter mais de 1 ano de serviço
+//     Estar no mínimo 6 meses trabalhando no cargo atual.
+
+// Imprima "Pode tirar férias" ou "Não pode tirar férias", dependendo do caso.
+
+let tempoDeServico = 12
+let minimoMesesTrabalhando = 3
+
+if (tempoDeServico >= 12 && minimoMesesTrabalhando >= 6) {
+    console.log("Pode tirar férias");
+} else {
+    console.log("Não pode tirar férias");
+}
+
+// QUESTÃO 10
+// Verificar se uma pessoa pode dirigir ou votar
+// Crie um programa que leia a idade de uma pessoa e verifique se ela pode dirigir ou votar. As regras são:
+
+//     A pessoa pode dirigir se tiver 18 anos ou mais.
+//     A pessoa pode votar se tiver 16 anos ou mais (voto opcional entre 16 e 17 anos e obrigatório a partir de 18 anos).
+
+// Imprima "Pode dirigir" se a pessoa puder dirigir, "Pode votar" se a pessoa puder votar, ou "Não pode nem dirigir nem votar" caso contrário.
+
+let idadePessoa = 18
+let podeVotar = false
+
+if (idadePessoa > 0){
+    if (idadePessoa >= 16 && idadePessoa <= 17 || idadePessoa >= 18 ) {
+        podeVotar = true;
+    }
+    
+    if (idadePessoa >= 18 && podeVotar) {
+        console.log("Pode dirigir e Pode votar");
+    } else if (idadePessoa >= 16 && idadePessoa <= 17 ) {
+        console.log("Pode Votar mas Não pode Dirigir");
+    } else {
+        console.log("Não pode nem dirigir nem votar");
+    }
+} else {
+    console.log("Idade inválida");
+}
+
