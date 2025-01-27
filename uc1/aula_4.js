4ec0475ff55cefda7a8d0b0a2ac585c3f664dafb
Alex Lanção <93793324+lancao2@users.noreply.github.com>
2025-01-27 14:50:30 -0300

    update to switch case

--- uc1/aula_4.js
+++ uc1/aula_4.js
@@ -2,20 +2,29 @@
 
 let numeroDaSemana = 10;
 
-if (numeroDaSemana == 1) {
-    console.log("Domingo");
-} else if (numeroDaSemana == 2) {
-    console.log("Segunda-feira");
-} else if (numeroDaSemana == 3) {
-    console.log("Terça-feira");
-} else if (numeroDaSemana == 4) {
-    console.log("Quarta-feira");
-} else if (numeroDaSemana == 5) {
-    console.log("Quinta-feira");
-} else if (numeroDaSemana == 6) {
-    console.log("Sexta-feira");
-} else if (numeroDaSemana == 7) {
-    console.log("Sábado");
-} else {
-    console.log("Número inválido. Informe um número de 1 a 7.");
+switch (numeroDaSemana) {
+    case 1:
+        console.log("Domingo");
+        break;
+    case 2:
+        console.log("Segunda-Feira");
+        break;
+    case 3:
+        console.log("Terça-Feira");
+        break;
+    case 4:
+        console.log("Quarta-Feira");
+        break;
+    case 5:
+        console.log("Quinta-Feira");
+        break;
+    case 6:
+        console.log("Sexta-Feira");
+        break;
+    case 7:
+        console.log("Sábado");
+        break;
+    default:
+        console.log("Número inválido. Informe um número de 1 a 7.");
+        break;  
 }
