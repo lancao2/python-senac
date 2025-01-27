c69cf5228d55b0fa5394001dc4616d48670f9f06
Alex Lanção <93793324+lancao2@users.noreply.github.com>
2025-01-27 14:30:48 -0300

    exercicios da aula 4

---
+++ uc1/aula_4.js
@@ -0,0 +1,21 @@
+// Crie um programa que para receber um numero de 1 a 7 e retornar o dia da semana correspondente.
+
+let numeroDaSemana = 10;
+
+if (numeroDaSemana == 1) {
+    console.log("Domingo");
+} else if (numeroDaSemana == 2) {
+    console.log("Segunda-feira");
+} else if (numeroDaSemana == 3) {
+    console.log("Terça-feira");
+} else if (numeroDaSemana == 4) {
+    console.log("Quarta-feira");
+} else if (numeroDaSemana == 5) {
+    console.log("Quinta-feira");
+} else if (numeroDaSemana == 6) {
+    console.log("Sexta-feira");
+} else if (numeroDaSemana == 7) {
+    console.log("Sábado");
+} else {
+    console.log("Número inválido. Informe um número de 1 a 7.");
+}
